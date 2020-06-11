2cfaae73ff7f6ca2d3b4b6c1aba7ce493fcc2d7a
hoswey <hongshuwei@gmail.com>
2020-06-11 22:38:50 +0800

    support chinese

--- ItermProfileSearcher.py
+++ ItermProfileSearcher.py
@@ -5,36 +5,35 @@ from lib import biplist
 import os
 import json
 import sys
-from workflow import Workflow, ICON_WARNING, MATCH_SUBSTRING
-
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-
+from workflow import Workflow3, ICON_WARNING, MATCH_SUBSTRING
 
 def read_profiles():
     # Read iTerm's preferences file
-    iTermPreferencesPath = os.path.join(os.environ["HOME"], "Library", "Preferences", "com.googlecode.iterm2.plist")
+    iTermPreferencesPath = os.path.join(os.environ["HOME"], "Library",
+                                        "Preferences",
+                                        "com.googlecode.iterm2.plist")
+
     plist = biplist.readPlist(iTermPreferencesPath)
     # Extract profile data from plist
     profiles = []
     for nb in plist['New Bookmarks']:
+        tags = " ".join(nb.get('Tags'))
         profiles.append({
             'name': nb.get('Name'),
             'command': nb.get('Command'),
             'icon': nb.get('Custom Icon Path'),
-            'tags': nb.get('Tags')
+            'tags': tags
         })
 
     return profiles
 
 
 def filter_key_for_profile(profile):
-    return profile['name'] + ' ' + str(profile['tags'])
+    return profile['name'] + profile['tags']
 
 
 def sort_key_for_profile(profile):
-    return profile['name']
+    return profile['name'] + profile['tags']
 
 
 def main(wf):
@@ -45,23 +44,23 @@ def main(wf):
     if len(wf.args):
         query = wf.args[0]
 
-    profiles = wf.filter(query, profiles, filter_key_for_profile, match_on=MATCH_SUBSTRING)
+    profiles = wf.filter(query, profiles, filter_key_for_profile,
+                         match_on=MATCH_SUBSTRING)
 
     if not profiles:
         wf.add_item('No profile matches', icon=ICON_WARNING)
-
-    profiles.sort(key=sort_key_for_profile)
-    for profile in profiles:
-        wf.add_item(title=profile['name'],
-                    subtitle=((str(profile['tags']) + ' ') if profile['tags'] else '') + profile['command'],
-                    arg=profile['name'],
-                    valid=True,
-                    icon=profile['icon'])
-
+    else:
+        profiles.sort(key=sort_key_for_profile)
+        for profile in profiles:
+            wf.add_item(title=profile['name'],
+                        subtitle=(profile['tags'] if profile['tags'] else '') + profile['command'],
+                        arg=profile['name'],
+                        valid=True,
+                        icon=profile['icon'])
     wf.send_feedback()
     return
 
 
-if __name__ == u"__main__":
-    wf = Workflow()
+if __name__ == "__main__":
+    wf = Workflow3()
     sys.exit(wf.run(main))
